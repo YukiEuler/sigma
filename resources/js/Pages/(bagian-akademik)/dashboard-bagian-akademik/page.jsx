ddf532c71fe7d1176ea65ea40d9858b6dc9eac1a
matahariann <dzusm@students.undip.ac.id>
2024-10-13 16:45:41 +0700

    bagian akademik

--- resources/js/Pages/(bagian-akademik)/dashboard-bagian-akademik/page.jsx
+++ resources/js/Pages/(bagian-akademik)/dashboard-bagian-akademik/page.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { usePage } from "@inertiajs/inertia-react";
 import BagianAkademikLayout from "../../../Layouts/BagianAkademikLayout";
 
 const DashboardBagianAkademik = () => {
+    const { props } = usePage();
+    const bagian_akademikData = props.bagian_akademik;
+    const [bagian_akademik, setBagian_akademik] = useState(bagian_akademikData);
+
+    useEffect(() => {
+        setBagian_akademik(bagian_akademikData);
+    }, [bagian_akademikData]);
     return (
-        <BagianAkademikLayout>
+        <BagianAkademikLayout bagian_akademik={bagian_akademik}>
             <h2>Dashboard Bagian Akademik</h2>
+            {bagian_akademik ? (
+                <div>
+                    <p>Welcome, {bagian_akademik.nama} <br />
+                    {bagian_akademik.nip} <br />
+                    </p>
+                    {/* Display other mahasiswa data as needed */}
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
         </BagianAkademikLayout>
     );
 };
