06da58378aa1c2b47b45e54f60e75768a98af57c
YukiEuler <110078841+YukiEuler@users.noreply.github.com>
2024-12-08 00:17:00 +0700

    fix: kaprodi sidebar

--- resources/js/Pages/(kaprodi)/data-mahasiswa/page.jsx
+++ resources/js/Pages/(kaprodi)/data-mahasiswa/page.jsx
@@ -85,7 +85,7 @@ const DataMahasiswa = () => {
     }, [dosenData, mahasiswaData]);
 
     return (
-        <KaprodiLayout dosen={dosen}>
+        <KaprodiLayout kaprodi={dosen}>
             <main className="flex-1 max-h-full">
                 <div className="flex flex-col items-start justify-between mt-2 pb-3 space-y-4 border-b lg:items-center lg:space-y-0 lg:flex-row">
                     <h1 className="text-2xl font-semibold whitespace-nowrap text-black">
